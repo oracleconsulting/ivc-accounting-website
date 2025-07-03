6f9cccf0e983a7326b69726a45814c794c06d627
James.Howard <James.Howard@MacBook-Pro-2.local>
2025-07-03 17:38:49 +0100

    Fix production CSS issues - add proper webpack and PostCSS config

--- next.config.js
+++ next.config.js
@@ -12,6 +12,14 @@ const nextConfig = {
       },
     ],
   },
+  // Ensure CSS modules work in production
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader'],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig; 
